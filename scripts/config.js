0f7db69cb8e849a310b9af4162825b8015e8c96e
j- <jh@live.com.au>
2014-10-11 01:13:25 +1100

    Upgraded okaylib to 0.1.1

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ requirejs.config({
 	paths: {
 		'jquery': '//code.jquery.com/jquery-1.11.1.min',
 		'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
-		'ok': '//cdn.rawgit.com/j-/ok/0.1.0/ok',
+		'ok': '//cdn.rawgit.com/j-/ok/0.1.1/ok',
 		'ok.views': '//cdn.rawgit.com/j-/ok/0.1.0/ok.views'
 	},
 	shim: {
