e1ea4f4aab0a2c679c95b9a717e0b1cb3918a0ef
j- <jh@live.com.au>
2014-10-12 18:15:47 +1100

    Added $View and require-text to config

--- scripts/config.js
+++ scripts/config.js
@@ -3,7 +3,9 @@ requirejs.config({
 		'jquery': '//code.jquery.com/jquery-1.11.1.min',
 		'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
 		'ok': '//cdn.rawgit.com/j-/ok/0.1.1/ok',
-		'ok.views': '//cdn.rawgit.com/j-/ok/0.1.0/ok.views'
+		'ok.views': '//cdn.rawgit.com/j-/ok/0.1.0/ok.views',
+		'ok.dollarview': '//cdn.rawgit.com/j-/ok/0.1.0/ok.dollarview',
+		'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text'
 	},
 	shim: {
 		'ok': {
