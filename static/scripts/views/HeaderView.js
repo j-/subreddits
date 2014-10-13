37097778f388b975bc2a50b90c2634e5c75980c5
j- <jh@live.com.au>
2014-10-13 12:54:42 +1100

    Added sorting trigger to header view

--- static/scripts/views/HeaderView.js
+++ static/scripts/views/HeaderView.js
@@ -1,11 +1,27 @@
 define(function (require) {
 	var ok = require('ok');
-	require('ok.views');
+	require('ok.dollarview');
+	var $ = require('jquery');
+	var _ = require('underscore');
 	var html = require('text!templates/Header.html');
-	var HeaderView = ok.View.extend({
+	var HeaderView = ok.$View.extend({
 		className: 'listing-controls navbar navbar-default navbar-fixed-top navbar-inverse',
 		render: function () {
-			this.el.innerHTML = html;
+			this.$el.html(html);
+		},
+		handleClickSort: function (e) {
+			e.preventDefault();
+			var $target = $(e.target);
+			var sort = $target.data('sort');
+			var t = $target.data('t');
+			this.trigger('sort', sort, t);
+		},
+		start: function () {
+			this.$el.on('click', 'a[data-sort]', _.bind(this.handleClickSort, this));
+		},
+		stop: function fn () {
+			fn.old.call(this);
+			this.$el.off('click', 'a[data-sort]');
 		}
 	});
 	return HeaderView;
