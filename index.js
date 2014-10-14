0e1695ccd67c52c7efa49d82a25bf80d00f03cc4
j- <jh@live.com.au>
2014-10-14 18:23:19 +1100

    Install libs with bower

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/assets'));
-app.use('/scripts', express.static(__dirname + '/scripts'));
 app.listen(3000);
--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,12 @@
 requirejs.config({
 	paths: {
-		'jquery': '//code.jquery.com/jquery-1.11.1.min',
-		'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
-		'ok': '//cdn.rawgit.com/j-/ok/0.1.1/ok',
-		'ok.views': '//cdn.rawgit.com/j-/ok/0.1.0/ok.views',
-		'ok.dollarview': '//cdn.rawgit.com/j-/ok/0.1.0/ok.dollarview',
-		'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
-		'bootstrap': '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min'
+		'jquery': '../bower_components/jquery/dist/jquery',
+		'underscore': '../bower_components/underscore/underscore',
+		'ok': '../bower_components/ok/ok',
+		'ok.views': '../bower_components/ok/ok.views',
+		'ok.dollarview': '../bower_components/ok/ok.dollarview',
+		'text': '../bower_components/text/text',
+		'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap'
 	},
 	shim: {
 		'ok': {
