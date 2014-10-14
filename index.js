f35e4de1d2062b61f0322dfcfe0287e020bbcb5a
j- <jh@live.com.au>
2014-10-14 17:52:57 +1100

    Moved scripts out of `static/` dir

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/assets'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
 app.listen(3000);
