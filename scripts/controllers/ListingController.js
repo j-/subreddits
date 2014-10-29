a4689091167fb879fc354bb6c209cb54946897e1
j- <jh@live.com.au>
2014-10-29 17:35:15 +1100

    Added state map to listing controller

--- scripts/controllers/ListingController.js
+++ scripts/controllers/ListingController.js
@@ -7,8 +7,11 @@ define(function (require) {
 		init: function (options) {
 			_.extend(this, _.pick(options, 'listing', 'router'));
 			_.bindAll(this, 'resetState', 'loadMore', 'handleResponse', 'handleRouteSubreddit', 'handleRouteFrontpage', 'handleRouteUsername', 'handleRouteDomain');
-			this.after = null;
-			this._currentPage = null;
+			this.state = new ok.Map({
+				after: null,
+				atEnd: false,
+				currentPage: null
+			});
 			this.latestXhr = null;
 			this.initRouter();
 		},
@@ -20,20 +23,11 @@ define(function (require) {
 			this.router.on('route:domain', this.handleRouteDomain);
 			this.router.parseCurrent();
 		},
-		setCurrentPage: function (currentPage) {
-			this._currentPage = currentPage;
-		},
-		getCurrentPage: function () {
-			return this._currentPage;
-		},
-		clearCurrentPage: function () {
-			this._currentPage = null;
-		},
 		resetState: function () {
 			this.listing.empty();
-			this.after = null;
-			this.end = false;
-			this.clearCurrentPage();
+			this.state.set('after', null);
+			this.state.set('atEnd', false);
+			this.state.set('currentPage', null);
 			if (this.latestXhr) {
 				this.latestXhr.abort();
 				this.latestXhr = null;
@@ -41,11 +35,11 @@ define(function (require) {
 		},
 		loadMore: function (options, callback) {
 			options = _.extend({
-				after: this.after,
-				page: this.getCurrentPage()
+				after: this.state.get('after'),
+				page: this.state.get('currentPage')
 			}, options);
 			callback = typeof callback === 'function' ? callback : _.identity;
-			if (this.end) {
+			if (this.state.get('atEnd')) {
 				callback(new Error('End of listing'));
 			}
 			if (this.latestXhr) {
@@ -67,9 +61,10 @@ define(function (require) {
 				this.handleError(err);
 				return;
 			}
-			this.after = response.data.after;
-			if (!this.after) {
-				this.end = true;
+			var after = response.data.after;
+			this.state.set('after', after);
+			if (!after) {
+				this.state.set('atEnd', true);
 				this.handleEnd();
 			}
 			this.listing.add(response.data.children);
@@ -77,22 +72,22 @@ define(function (require) {
 		},
 		handleRouteFrontpage: function () {
 			this.resetState();
-			this.setCurrentPage('/');
+			this.state.set('currentPage', '/');
 			this.loadMore();
 		},
 		handleRouteSubreddit: function (subreddit) {
 			this.resetState();
-			this.setCurrentPage('/r/' + subreddit);
+			this.state.set('currentPage', '/r/' + subreddit);
 			this.loadMore();
 		},
 		handleRouteUsername: function (username) {
 			this.resetState();
-			this.setCurrentPage('/user/' + username);
+			this.state.set('currentPage', '/user/' + username);
 			this.loadMore();
 		},
 		handleRouteDomain: function (domain) {
 			this.resetState();
-			this.setCurrentPage('/domain/' + domain);
+			this.state.set('currentPage', '/domain/' + domain);
 			this.loadMore();
 		}
 	});
