e4c43b56a664a39948b3e69086e5b377e5567c7f
j- <jh@live.com.au>
2014-10-29 19:02:27 +1100

    Added `AccountModel#isDeleted()`

--- scripts/models/AccountModel.js
+++ scripts/models/AccountModel.js
@@ -1,11 +1,15 @@
 define(function (require) {
 	var ThingModel = require('models/ThingModel');
+	var DELETED = '[deleted]';
 	var AccountModel = ThingModel.extend({
 		defaults: {
 			kind: 't2'
 		},
 		getUserPageURL: function () {
 			return 'http://www.reddit.com/user/' + this.get('name');
+		},
+		isDeleted: function () {
+			return this.get('name') === DELETED;
 		}
 	});
 	return AccountModel;
