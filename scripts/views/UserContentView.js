0b592394e5a28189974e905f8a2c29cce43b9d80
j- <jh@live.com.au>
2014-10-30 15:00:23 +1100

    Replace user and subreddit links with inline views
    Views are not started and stopped with the user content view.
    This will have to be addressed at some point.

--- scripts/views/UserContentView.js
+++ scripts/views/UserContentView.js
@@ -2,12 +2,54 @@ define(function (require) {
 	var ok = require('ok');
 	require('ok.dollarview');
 	var _ = require('underscore');
+	var AccountModel = require('models/AccountModel');
+	var SubredditModel = require('models/SubredditModel');
+	var InlineUserView = require('views/InlineUserView');
+	var InlineSubredditView = require('views/InlineSubredditView');
 	var UserContentView = ok.$View.extend({
 		className: 'user-content',
 		render: function () {
 			var html = this.watch.get();
 			html = _.unescape(html);
 			this.$el.html(html);
+			this.replaceLinks();
+		},
+		replaceLinks: function () {
+			var context = this;
+			this.$('a').each(function () {
+				var $a = $(this);
+				context.processLink($a);
+			});
+		},
+		processLink: function ($a) {
+			var href = String($a.attr('href'));
+			var substr = href.substring(0, 3);
+			if (substr === '/u/') {
+				this.replaceUserLink($a);
+			}
+			else if (substr === '/r/') {
+				this.replaceSubredditLink($a);
+			}
+		},
+		replaceUserLink: function ($a) {
+			var model = new AccountModel({
+				name: $a.attr('href').substring(3)
+			});
+			var view = new InlineUserView({
+				watch: model
+			});
+			view.render();
+			$a.replaceWith(view.$el);
+		},
+		replaceSubredditLink: function ($a) {
+			var model = new SubredditModel({
+				display_name: $a.attr('href').substring(3)
+			});
+			var view = new InlineSubredditView({
+				watch: model
+			});
+			view.render();
+			$a.replaceWith(view.$el);
 		}
 	});
 	return UserContentView;
