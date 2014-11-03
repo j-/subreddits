e499921196407d8a4325844dc6d6f22897a4af1b
j- <jh@live.com.au>
2014-11-03 21:38:45 +1100

    GfyItem view now inherits from Resizable view

--- scripts/views/GfyItemView.js
+++ scripts/views/GfyItemView.js
@@ -1,11 +1,12 @@
 define(function (require) {
-	var ok = require('ok');
-	require('ok.views');
-	var GfyItemView = ok.View.extend({
+	var ResizableView = require('views/ResizableView');
+	var GfyItemView = ResizableView.extend({
+		tagName: 'a',
 		render: function () {
 			this.empty();
 			var video = document.createElement('video');
 			var id = this.watch.get('gfyName');
+			var url = 'http://gfycat.com/' + id;
 			video.autoplay = true;
 			video.loop = true;
 			video.muted = true;
@@ -13,7 +14,9 @@ define(function (require) {
 			video.preload = true;
 			video.poster = 'http://thumbs.gfycat.com/' + id + '-poster.jpg';
 			video.src = this.watch.get('webmUrl');
-			this.el.appendChild(video);
+			this.$el
+				.attr('href', url)
+				.append(video);
 		},
 		play: function () {
 			var video = this.el.getElementsByTagName('video')[0];
