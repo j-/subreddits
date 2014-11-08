6238e9fdc41230ca214b5a6af7489634676255c5
j- <jh@live.com.au>
2014-11-08 17:47:03 +1100

    Added comments view

---
+++ scripts/views/CommentsView.js
@@ -0,0 +1,10 @@
+define(function (require) {
+	var ok = require('ok');
+	require('ok.dollarview');
+	var CommentsView = ok.$View.extend({
+		render: function () {
+			// to be implemented
+		}
+	});
+	return CommentsView;
+});
--- scripts/views/MainView.js
+++ scripts/views/MainView.js
@@ -2,11 +2,12 @@ define(function (require) {
 	var ok = require('ok');
 	require('ok.dollarview');
 	var InfiniteScrollListingView = require('views/InfiniteScrollListingView');
+	var CommentsView = require('views/CommentsView');
 	var MainView = ok.$View.extend({
 		className: 'main-view',
 		init: function (options) {
 			_.extend(this, _.pick(options, 'router', 'controller', 'listing'));
-			_.bindAll(this, 'scrollCallback');
+			_.bindAll(this, 'scrollCallback', 'handleRouteListing', 'handleRouteComments');
 			this.listingView = this.addChildView(InfiniteScrollListingView, {
 				id: 'listing',
 				watch: this.listing,
@@ -14,11 +15,48 @@ define(function (require) {
 				scrollThreshold: 1200,
 				scrollCallback: this.scrollCallback
 			});
+			this.commentsView = this.addChildView(CommentsView, {
+				id: 'comments',
+				controller: this.controller
+			});
+			this.routeState = new ok.Property();
+			this.listenTo(this.routeState, 'change', this.renderCurrentRoute);
+			this.router.on('route:subreddit', this.handleRouteListing);
+			this.router.on('route:frontpage', this.handleRouteListing);
+			this.router.on('route:username', this.handleRouteListing);
+			this.router.on('route:multi', this.handleRouteListing);
+			this.router.on('route:domain', this.handleRouteListing);
+			this.router.on('route:search', this.handleRouteListing);
+			this.router.on('route:comments', this.handleRouteComments);
 		},
 		render: function () {
 			this.empty();
 			this.renderChildViews();
+			this.renderCurrentRoute();
+		},
+		renderCurrentRoute: function () {
+			switch (this.routeState.get()) {
+				case 'listing':
+					this.showListing();
+					break;
+				case 'comments':
+					this.showComments();
+					break;
+			}
+		},
+		handleRouteListing: function () {
+			this.routeState.set('listing');
+		},
+		handleRouteComments: function () {
+			this.routeState.set('comments');
+		},
+		showListing: function () {
+			this.empty();
 			this.$el.append(this.listingView.$el);
+		},
+		showComments: function () {
+			this.empty();
+			this.$el.append(this.commentsView.$el);
 		},
 		scrollCallback: function (done) {
 			if (!this.controller.end) {
