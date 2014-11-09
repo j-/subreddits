f7f78a41e8692c7db77b7ae87ee52f2b035da776
j- <jh@live.com.au>
2014-11-09 17:41:15 +1100

    Added link view to comments view

--- scripts/models/ThingModel.js
+++ scripts/models/ThingModel.js
@@ -2,7 +2,7 @@ define(function (require) {
 	var ok = require('ok');
 	var ThingModel = ok.Map.extend({
 		setMap: function (attrs) {
-			if (typeof attrs.kind === 'string' && attrs.data) {
+			if (attrs && typeof attrs.kind === 'string' && attrs.data) {
 				return ok.Map.prototype.setMap.call(this, attrs.data);
 			}
 			return ok.Map.prototype.setMap.call(this, attrs);
--- scripts/views/CommentsView.js
+++ scripts/views/CommentsView.js
@@ -1,9 +1,17 @@
 define(function (require) {
 	var ok = require('ok');
 	require('ok.dollarview');
+	var LinkView = require('views/LinkView');
 	var CommentsView = ok.$View.extend({
+		init: function () {
+			this.linkView = this.addChildView(LinkView, {
+				watch: this.watch.property('link')
+			});
+		},
 		render: function () {
-			// to be implemented
+			this.empty();
+			this.renderChildViews();
+			this.$el.append(this.linkView.$el);
 		}
 	});
 	return CommentsView;
